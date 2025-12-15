e15eff6b3aca109be13d96ff219152a1e568b3a3
Daniel Santos Lourenco <dasantos@c1r9s7.42porto.com>
2025-12-15 11:02:31 +0000

    shell

---
+++ discovery_piscine/cell04/ex04/argv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+
+count=0
+for arg in "$@"; do
+    echo "$arg"
+    count=$((count + 1))
+    [ $count -eq 3 ] && break
+done
+
---
+++ discovery_piscine/cell05/ex05/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+
+for arg in "$@"; do
+    mkdir -p "ex$arg"
+done
+
