358e344128ab84e85a3ca2ff2f333a8395feea67
Daniel Lourenço <contact.loureno18@gmail.com>
2025-12-15 13:15:15 +0000

    30 done

---
+++ discovery_piscine/cell30/ex00/background.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+  if (document.body.style.backgroundColor == "lightblue") {
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.body.style.backgroundColor = "lightblue";
+  }
+}
